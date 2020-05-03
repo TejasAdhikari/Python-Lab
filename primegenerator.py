81a7ba573059781c43ab4183a1dd52defcbda76f
TejasAdhikari <tejasadhikari123@gmail.com>
2020-05-03 17:14:29 +0530

    experiment 9

--- primegenerator.py
+++ primegenerator.py
@@ -1,16 +1,14 @@
 T = int(input("Enter the number of test cases: "))
 while T!=0:
     m, n = map(int, input("Enter m and n: ").split())
-    l, marked, p = [*range(m, n+1)], [], []
+    l, marked, p, g = [*range(2, n+1)], [], [], [*range(m, n+1)]
     for i in l:
-        if (i**2 in l) and i!=1:
+        if i**2 in l:
             for j in l[l.index(i**2):]:
                 if j%i == 0:
                     marked.append(j)
-        elif i==1:
-            continue
         else:
             break
-    p = [x for x in l if (x not in marked) and x!=1]
+    p = [x for x in g if (x not in marked) and x!=1]
     print("Prime Numbers are", p)
     T-=1
